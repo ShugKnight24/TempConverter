72c6bff3fa171ceb5636df54c4b9fe9e9f2490e7
Shugmi Shumunov <SShumunov@gmail.com>
2016-07-05 21:30:31 -0400

    Changed some of the JS and added some CSS.

--- scripts.js
+++ scripts.js
@@ -3,33 +3,36 @@
   var tempCelsius;
 
   if (tempFahrenheit === 100) {
-  console.log ((100 - 32)*(5/9));} \
-*/
+  console.log ((100 - 32)*(5/9));} */
 
+/*From Fahrenheit to Celsius*/
 function fChange() {
   var input = document.getElementById("fTemp");
   var text = input.value;
-  if ("fTemp") {
+
+  if (!text)  {
+    alert("You have to enter a number for it to work first!");
+    return;
+  } else if ("fTemp") {
     document.getElementById("celsiusresult").innerHTML = input.value = ("The temperature is " + ((text - 32)*(5/9)) + " Degrees Celsius.");
     cTemp.value="";
     fTemp.value="";
   }
 }
 
+/*From Celsius to Fahrenheit*/
 function cChange() {
   var input = document.getElementById("cTemp");
   var text = input.value;
-  if ("cTemp") {
+  if (!text)  {
+    alert("You have to enter in a number for it to work first!");
+    return;
+  } else if ("cTemp") {
    document.getElementById("fahrenheitresult").innerHTML = input.value = ("The temperature is " + ((text * (9/5)) + 32) + " Degrees Fahrenheit.");
    cTemp.value="";
    fTemp.value="";
   }
  }
-// if (!text)  {
-//   alert("Hey, you have to enter in a number for it to work first silly!");
-//   return;
-// }
-
 
 /* To test for later
 convertTemp(212, "C"); // > 100
